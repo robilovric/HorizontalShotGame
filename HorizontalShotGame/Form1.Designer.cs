e5b12172fbed7d2192a4769caa7ef2bd6f0bc46c
robilovric <robilovric7@gmail.com>
2022-02-10 21:43:42 +0100

    Add project files.

---
+++ HorizontalShotGame/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace HorizontalShotGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.ObjectBall = new System.Windows.Forms.PictureBox();
+            this.Target = new System.Windows.Forms.PictureBox();
+            this.GameTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.ObjectBall)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Target)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ObjectBall
+            // 
+            this.ObjectBall.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.ObjectBall.Image = ((System.Drawing.Image)(resources.GetObject("ObjectBall.Image")));
+            this.ObjectBall.Location = new System.Drawing.Point(10, 10);
+            this.ObjectBall.Name = "ObjectBall";
+            this.ObjectBall.Size = new System.Drawing.Size(22, 22);
+            this.ObjectBall.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.ObjectBall.TabIndex = 0;
+            this.ObjectBall.TabStop = false;
+            // 
+            // Target
+            // 
+            this.Target.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.Target.Image = ((System.Drawing.Image)(resources.GetObject("Target.Image")));
+            this.Target.Location = new System.Drawing.Point(688, 373);
+            this.Target.Name = "Target";
+            this.Target.Size = new System.Drawing.Size(87, 65);
+            this.Target.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.Target.TabIndex = 1;
+            this.Target.TabStop = false;
+            // 
+            // GameTimer
+            // 
+            this.GameTimer.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(787, 450);
+            this.Controls.Add(this.Target);
+            this.Controls.Add(this.ObjectBall);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.Text = "Horizontal Shot Game";
+            ((System.ComponentModel.ISupportInitialize)(this.ObjectBall)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Target)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox ObjectBall;
+        private System.Windows.Forms.PictureBox Target;
+        private System.Windows.Forms.Timer GameTimer;
+
+      
+    }
+}
+
+ 
---
+++ HorizontalShotGame/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace HorizontalShotGame
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            ResetGame();
+        }
+
+        double gravity = 9.81;
+        double velocity = 0;
+        Random rand = new Random();
+
+       
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int tick_number = 1;
+            LeaveTrail();
+            ObjectBall.Left += (int)(velocity * 1 * tick_number);
+            ObjectBall.Top += (int)(0.5 * gravity * Math.Pow(1 * tick_number, 2));
+            tick_number += 1;
+        }
+
+        private void LeaveTrail()
+        {
+            PictureBox trail = new PictureBox();
+            trail.Height = 2;
+            trail.Width = 2;
+            trail.BackColor = Color.Black;
+            trail.Location = ObjectBall.Location;
+            this.Controls.Add(trail);
+        }
+
+        private void ResetGame()
+        {
+            GameTimer.Start();
+            int x = rand.Next(20, this.ClientSize.Width - Target.Width);
+            int y = rand.Next(20, this.ClientSize.Height - Target.Height);
+            Target.Location = new Point(x, y);
+        }
+    }
+
+}
